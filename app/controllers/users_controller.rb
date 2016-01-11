3151f6379c95f9cf8d0a90be86816af9c8b5c2bc
Mimi Schatz <mcschatz@gmail.com>
2016-01-11 10:33:20 -0700

    can create a user and track a user current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages.join(", ")
+      render :new
+    end
+  end
 
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
