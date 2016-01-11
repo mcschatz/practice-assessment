42a5fd2ea21b535442e705fb4b069eaaadc42b19
Mimi Schatz <mcschatz@gmail.com>
2016-01-11 09:36:07 -0700

    fixed spelling error and added path assertion

--- test/integration/user_creation_test.rb
+++ test/integration/user_creation_test.rb
@@ -4,7 +4,10 @@ class UserCreationTest < ActionDispatch::IntegrationTest
   include Capybara::DSL
 
   test 'a user can be created' do
-    vist root_path
+    visit root_path
+    click_button "Sign Up"
+    assert_current_path(user_path)
+
     fill_in "Username", with: "mimi"
     fill_in "Password", with: "password"
     click_button "Create Account"
